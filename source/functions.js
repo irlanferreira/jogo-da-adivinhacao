f642bba1379281ef0382d01dc687d6cee539cda6
Irlan Ferreira <97058040+irlanferreira@users.noreply.github.com>
2022-01-08 18:48:22 -0300

    Release - 1.0

--- source/functions.js
+++ source/functions.js
@@ -1,4 +1,5 @@
 function randomNumber(min, max){
+    //Gera um número aleatório
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 function reiniciar(){
@@ -7,30 +8,36 @@ function reiniciar(){
 function jogar(){
     document.querySelector('#num').focus()
     let num = Number(document.querySelector('#num').value)
+    //Validação do valor
     if(num >= 1 && num <= 100) {
         divresultado.style.display = 'block'
-    if(num == aleatorio){
-        divresultado.innerHTML = `<p>Parabéns, você acertou :D</p> <p style="font-size: 1.5em">${aleatorio}</p> <p><input type="button" value="Reiniciar" onclick="reiniciar()" id="botaoreiniciar">`
-        divresultado.style.backgroundColor = '#00800066'
-        botao.style.display = 'none'
-    }else if(num > aleatorio){
-        divresultado.innerHTML = '<p>O valor que foi gerado é menor.</p>'
-        tentativas --
-    }else if(num < aleatorio){
-        divresultado.innerHTML = '<p>O valor que foi gerado é maior.</p>'
-        tentativas --
-    }
-    if(num != aleatorio){
-        divtentativas.style.display = 'block'
-        divtentados.innerHTML += `${num} `
-        divresultado.innerHTML += `<p>Tentativas restantes: ${tentativas}</p>`
-        if(tentativas == 0){
-            alert('Suas tentativas acabaram. Seu jogo será reiniciado.')
-            window.location.reload()
+        if(num == aleatorio){
+            //Caso o valor seja igual a resposta certa
+            divresultado.innerHTML = `<p>Parabéns, você acertou :D</p> <p style="font-size: 1.5em">${aleatorio}</p> <p><input type="button" value="Reiniciar" onclick="reiniciar()" id="botaoreiniciar">`
+            divresultado.style.backgroundColor = '#00800066'
+            botao.style.display = 'none'
+        }else if(num > aleatorio){
+            //Caso o valor deja maior do que a resposta certa
+            divresultado.innerHTML = '<p>O valor que foi gerado é menor.</p>'
+            tentativas --
+        }else if(num < aleatorio){
+            //Caso o valor seja menor do que a resposta certa
+            divresultado.innerHTML = '<p>O valor que foi gerado é maior.</p>'
+            tentativas --
         }
-    }
-    }else{
-        alert('Você só pode inserir números de 1 a 100.')
-    }
+        if(num != aleatorio){
+            //Caso o valor seja diferente da resposta certa
+            divtentativas.style.display = 'block'
+            divtentados.innerHTML += `${num} `
+            divresultado.innerHTML += `<p>Tentativas restantes: ${tentativas}</p>`
+            if(tentativas == 0){
+                //Caso as tentativas acabem
+                alert('Suas tentativas acabaram. Seu jogo será reiniciado.')
+                window.location.reload()
+            }
+        }
+        }else{
+            alert('Você só pode inserir números de 1 a 100.')
+        }
     
 }
