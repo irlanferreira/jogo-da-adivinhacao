c49b24c658635ad51365055a0ac39659fb7bb5bc
Irlan Ferreira <97058040+irlanferreira@users.noreply.github.com>
2022-01-08 19:16:48 -0300

    O número de tentativas foi diminuído para 5.

--- source/script.js
+++ source/script.js
@@ -2,6 +2,6 @@ var aleatorio = randomNumber(1, 100)
 var divresultado = document.querySelector('#resultado')
 var divtentativas = document.querySelector('#tentativas')
 var divtentados = document.querySelector('#numerosinseridos')
-var tentativas = 10
+var tentativas = 5
 var botao = document.querySelector('#botao')
 var caixanum = document.querySelector('#num')
